da638600677650b34640029387c60f9958c8ad70
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 23:55:12 +0900

    form : add missing function comment

--- src/main/java/com/portfolio/sns/repository/AuthRepository.java
+++ src/main/java/com/portfolio/sns/repository/AuthRepository.java
@@ -5,6 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * The interface Auth repository.
+ */
 public interface AuthRepository extends JpaRepository<User, Integer> {
+    /**
+     * @param username
+     * @return optional
+     * @methodName : findByUsername
+     * @author : rulethecode9060
+     * @date : 2023.07.13
+     * @description : 회원 ID로 회원 조회
+     */
     Optional<User> findByUsername(String username);
 }
