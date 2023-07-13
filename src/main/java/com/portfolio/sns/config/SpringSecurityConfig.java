cfb1835b19ade8efdffba912d3a3e34e99e1c442
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 22:31:27 +0900

    feat : add BCryptPasswordEncoder bean for user password crypt

--- src/main/java/com/portfolio/sns/config/SpringSecurityConfig.java
+++ src/main/java/com/portfolio/sns/config/SpringSecurityConfig.java
@@ -1,13 +1,19 @@
 package com.portfolio.sns.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
     @Override
