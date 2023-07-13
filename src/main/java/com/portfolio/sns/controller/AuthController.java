7f6a706ee1a847a5b04068e893f6611e62671547
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 22:53:54 +0900

    feat : add function to move sign in page

--- src/main/java/com/portfolio/sns/controller/AuthController.java
+++ src/main/java/com/portfolio/sns/controller/AuthController.java
@@ -3,11 +3,17 @@ package com.portfolio.sns.controller;
 import com.portfolio.sns.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
-    
+    @GetMapping("/auth/signin")
+    public String signInPage(){
+        return "auth/signin";
+    }
+
+
 }
