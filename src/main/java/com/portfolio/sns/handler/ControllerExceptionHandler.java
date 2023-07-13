b1a6511f7b1bd5d09350bb0c2a25f5fba5232360
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 23:27:02 +0900

    feat : add generic validation function

--- src/main/java/com/portfolio/sns/handler/ControllerExceptionHandler.java
+++ src/main/java/com/portfolio/sns/handler/ControllerExceptionHandler.java
@@ -6,8 +6,25 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The type Controller exception handler.
+ */
 @ControllerAdvice
 @RestController
 public class ControllerExceptionHandler {
+
+    /**
+     * @param e
+     * @return string
+     * @methodName : validationException
+     * @author : rulethecode9060
+     * @date : 2023.07.13
+     * @description : 일반적인 유효성 검증 실패 처리 핸들러
+     */
+    @ExceptionHandler(CustomValidationException.class)
+    public String validationException(CustomValidationException e){
+        return Script.back(e.getMessage());
+    }
+
 
 }
