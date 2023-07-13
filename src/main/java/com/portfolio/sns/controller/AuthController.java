a3e8355f823da9f901f3859be7e3fab28f9cee54
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 23:08:02 +0900

    feat : add function to request sign up

--- src/main/java/com/portfolio/sns/controller/AuthController.java
+++ src/main/java/com/portfolio/sns/controller/AuthController.java
@@ -1,9 +1,18 @@
 package com.portfolio.sns.controller;
 
+import com.portfolio.sns.dto.auth.SignUpDto;
+import com.portfolio.sns.exception.CustomValidationException;
 import com.portfolio.sns.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +27,19 @@ public class AuthController {
     @GetMapping("/auth/signup")
     public String signUpPage(){
         return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(@Valid SignUpDto signUpDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error : bindingResult.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationException("회원 가입 유효성 검증 실패", errorMap);
+        }
+        authService.signup(signUpDto.toEntity());
+        return "redirect:/auth/signin";
     }
 
 
