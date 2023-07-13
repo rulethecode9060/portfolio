9a77c9a07b0086aa8c17d444417a2f5a2b77dd0b
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 22:22:13 +0900

    feat : add entity for user

---
+++ src/main/java/com/portfolio/sns/domain/User.java
@@ -0,0 +1,86 @@
+package com.portfolio.sns.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * The type User.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+    /**
+     * PK값
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    /**
+     * 계정 ID
+     */
+    @Column(unique = true, length = 20, nullable = false) // 계정 ID는 중복이 없어야 하므로
+    private String username;
+    /**
+     * 계정 패스워드
+     */
+    @Column(nullable = false)
+    private String password;
+    /**
+     * 이름
+     */
+    @Column(nullable = false)
+    private String name;
+    /**
+     * 홈페이지 주소
+     */
+    private String website;
+    /**
+     * 자기소개
+     */
+    private String bio;
+    /**
+     * E-mail 주소
+     */
+    @Column(nullable = false)
+    private String email;
+    /**
+     * 핸드폰 번호
+     */
+    private String phone;
+    /**
+     * 성별
+     */
+    private String gender;
+    /**
+     * 가입 일자
+     */
+    private LocalDateTime createDate;
+    /**
+     * 프로필 사진 URL
+     */
+    private String profileImageUrl;
+    /**
+     * 권한
+     */
+    private String role;
+
+    /**
+     * @methodName : createDate
+     * @author : rulethecode9060
+     * @date : 2023.07.13
+     * @description : DB INSERT 작업 전에 createDate를 현재시간값으로 초기화
+     */
+    @PrePersist // DB INSERT 작업 전 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+
+}
