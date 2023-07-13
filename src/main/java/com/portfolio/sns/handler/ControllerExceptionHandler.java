60f95c9bbf48859a62729c6630a1a2a63d967c72
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 23:22:11 +0900

    feat : add exception handler for controller

---
+++ src/main/java/com/portfolio/sns/handler/ControllerExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.portfolio.sns.handler;
+
+import com.portfolio.sns.exception.CustomValidationException;
+import com.portfolio.util.Script;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class ControllerExceptionHandler {
+
+}
