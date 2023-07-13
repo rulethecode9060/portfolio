8ae3802d2b09afb653b95b86b316f2c0885cf0c9
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-14 00:02:48 +0900

    feat : add handler for auth failure

---
+++ src/main/java/com/portfolio/sns/handler/auth/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,22 @@
+package com.portfolio.sns.handler.auth;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("로그인 실패");
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
