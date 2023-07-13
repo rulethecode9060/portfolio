1c5fa316af8784fcb0b043d10ce0c69c61b14543
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-13 23:54:04 +0900

    feat : add find user by username function

--- src/main/java/com/portfolio/sns/repository/AuthRepository.java
+++ src/main/java/com/portfolio/sns/repository/AuthRepository.java
@@ -3,6 +3,8 @@ package com.portfolio.sns.repository;
 import com.portfolio.sns.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+public interface AuthRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
