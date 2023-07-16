aa8fa0238c2d8b85b5626b98a75562354e7a5eb9
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-17 02:40:20 +0900

    feat : Add api exception and api validation exception handling handlers

--- src/main/java/com/portfolio/sns/handler/ControllerExceptionHandler.java
+++ src/main/java/com/portfolio/sns/handler/ControllerExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.portfolio.sns.handler;
 
+import com.portfolio.sns.dto.common.CommonResponseDto;
+import com.portfolio.sns.exception.CustomAPIException;
 import com.portfolio.sns.exception.CustomException;
 import com.portfolio.sns.exception.CustomValidationException;
 import com.portfolio.util.Script;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,32 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public String exception(CustomException e){
         return Script.back(e.getMessage());
+    }
+
+    /**
+     * @param e
+     * @return responseEntity entity
+     * @methodName : apiException
+     * @author : rulethecode9060
+     * @date : 2023.07.16
+     * @description : API 유효성 검증 실패 처리 핸들러
+     */
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> apiValidationException(CustomValidationException e){
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * @param e
+     * @return response entity
+     * @methodName : apiException
+     * @author : rulethecode9060
+     * @date : 2023.07.16
+     * @description : 일반적인 API 커스텀 예외 처리 핸들러
+     */
+    @ExceptionHandler(CustomAPIException.class)
+    public ResponseEntity<?> apiException(CustomAPIException e){
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
     }
 
 
