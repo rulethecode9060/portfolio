48c6b1eefc92a0741de50464c6277429a4aa8acd
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-21 03:30:28 +0900

    feat : Add like, unlike function

--- src/main/java/com/portfolio/sns/api/ImageApiController.java
+++ src/main/java/com/portfolio/sns/api/ImageApiController.java
@@ -4,6 +4,7 @@ import com.portfolio.sns.domain.Image;
 import com.portfolio.sns.dto.auth.PrincipalDetails;
 import com.portfolio.sns.dto.common.CommonResponseDto;
 import com.portfolio.sns.service.ImageService;
+import com.portfolio.sns.service.LikesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,8 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +24,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ImageApiController {
     private final ImageService imageService;
+    private final LikesService likesService;
 
     /**
      * @param principalDetails
@@ -39,6 +40,36 @@ public class ImageApiController {
     , @PageableDefault(size = 3) Pageable pageable){
         Page<Image> images = imageService.story(principalDetails.getUser().getId(), pageable);
         return new ResponseEntity<>(new CommonResponseDto<>(1, "스토리 이미지 불러오기 성공", images), HttpStatus.OK);
+    }
+
+    /**
+     * @param imageId
+     * @param principalDetails
+     * @return response entity
+     * @methodName : likes
+     * @author : rulethecode9060
+     * @date : 2023.07.20
+     * @description : 좋아요 등록 요청 API
+     */
+    @PostMapping("/api/image/{imageId}/likes")
+    public ResponseEntity<?> likes(@PathVariable int imageId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        likesService.likes(imageId, principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CommonResponseDto<>(1, "좋아요 성공", null), HttpStatus.CREATED);
+    }
+
+    /**
+     * @param imageId
+     * @param principalDetails
+     * @return response entity
+     * @methodName : unLikes
+     * @author : rulethecode9060
+     * @date : 2023.07.20
+     * @description : 좋아요 취소 요청 API
+     */
+    @DeleteMapping("/api/image/{imageId}/likes")
+    public ResponseEntity<?> unLikes(@PathVariable int imageId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        likesService.unLikes(imageId, principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CommonResponseDto<>(1, "좋아요 취소 성공", null), HttpStatus.OK);
     }
 
 }
