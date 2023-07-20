cbabcb887f8bec906912468247282c87f7902d6f
rulethecode9060 <rule.the.code.9060@gmail.com>
2023-07-21 03:28:05 +0900

    feat : Add like, unlike function

---
+++ src/main/java/com/portfolio/sns/repository/LikesRepository.java
@@ -0,0 +1,39 @@
+package com.portfolio.sns.repository;
+
+import com.portfolio.sns.domain.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * The interface Likes repository.
+ */
+public interface LikesRepository extends JpaRepository<Likes, Integer> {
+
+
+    /**
+     * @param imageId
+     * @param principalId
+     * @return int
+     * @methodName : likes
+     * @author : rulethecode9060
+     * @date : 2023.07.20
+     * @description : 좋아요 등록
+     */
+    @Modifying
+    @Query(value = "INSERT INTO likes(imageId, userId, createDate) VALUES(:imageId, :principalId, now())", nativeQuery = true)
+    int likes(int imageId, int principalId);
+
+    /**
+     * @param imageId
+     * @param principalId
+     * @return int
+     * @methodName : unLikes
+     * @author : rulethecode9060
+     * @date : 2023.07.20
+     * @description : 좋아요 취소
+     */
+    @Modifying
+    @Query(value = "DELETE FROM likes WHERE imageId = :imageId AND userId = :principalId", nativeQuery = true)
+    int unLikes(int imageId, int principalId);
+}
